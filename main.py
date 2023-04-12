507e338f3a26bed6140263c390ad57e6f1bc96e8
DieMP3 <130253828+DieMP3@users.noreply.github.com>
2023-04-12 18:21:31 -0300

    Enviando archivo main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+valor1 = float(input("Ingrese el primer valor: "))
+valor2 = float(input("Ingrese el segundo valor: "))
+
+resultado = valor1 + valor2
+
+print("El resultado de la suma es:", resultado)
